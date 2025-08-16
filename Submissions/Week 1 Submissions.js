614ecad8af89c1ae7086fda9f55f46cfc5223c9e
njbeepbeep <njbeepbeep@icloud.com>
2025-08-16 12:18:08 +1000

    Submission 1

--- Submissions/Week 1 Submissions.js
+++ Submissions/Week 1 Submissions.js
@@ -1,10 +1,6 @@
- 3 + 4;
-// have to name 10000 cats but i do not have enough names well lets start with numbers
-
- var drawCats = function (howManyTimes) {
-  for (var i = 0; i < howManyTimes; i++) {
-    console.log(i + " =^.^=");
-  }
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + "=^.^=")
+    }
 };
-
-drawCats(10000); 
+drawCats(10);
